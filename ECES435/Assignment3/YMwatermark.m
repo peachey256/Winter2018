a494afb2ab752ea90288b619ea3f142f90990d24
Sarah Peachey <Peachey@n3-1-137.dhcp.drexel.edu>
2018-02-09 17:05:40 -0500

    yong mintz

--- ECES435/Assignment3/YMwatermark.m
+++ ECES435/Assignment3/YMwatermark.m
@@ -2,14 +2,18 @@ function [marked] = YMwatermark( img,wmk,key )
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
     rng(key); 
-    LUTvals= rand(1,256) > .5;
+    LUTvals= repmat([0 1]',128,1); %rand(1,256) > .5;
     [r,c]=size(img); 
     marked=zeros(size(img)); 
-    incr=[1, -1, 2, -2, 3, -3, 4, -4, 5, -5]; 
+    incr=[1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -1, 7, -7, 8, -8, 9, -9, 10, ...
+            -10, 11, -11, 12, -12, 13, -13, 14, -14, 15, -15]; 
     for i=1:r
         for j=1:c
             k=0; 
             l=0; 
+            if img(i,j)==0
+                img(i,j)=1; % can't index by zero in matlab 
+            end 
             while(LUTvals(mod((img(i,j)+k),255))~=wmk(i,j))
                 l=l+1; 
                 k=incr(l); 
@@ -21,9 +25,8 @@ function [marked] = YMwatermark( img,wmk,key )
             marked(i,j)=img(i,j)+k; 
         end 
     end
-    error=img-marked
     
-    
+    marked = uint8(marked); 
 
 end
 
