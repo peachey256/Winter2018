8d0874ae4bafd2c6887e8501051dcf1942a2313b
Sarah Peachey <Peachey@n3-1-137.dhcp.drexel.edu>
2018-02-16 11:59:06 -0500

    Asssignment 4 part 2

---
+++ ECES435/Assignment4/blockDetect.m
@@ -0,0 +1,42 @@
+function [ k ] = blockDetect( im )
+%blockDetect implements the Fan and de Quieroz?s JPEG blocking artifact
+    % detecting algorithm. inputs any image and output the k, blcoking 
+    % strength value. 
+    Zp=[]; 
+    Zpp=[];
+    [r,c]=size(im); 
+    for i=1:8:r-8 % dont do the last 8x8 block in row or cols 
+        for j=1:8:c-8
+            grid=im(i:i+7,j:j+7); % grid plus one 
+            A=grid(4,4); 
+            B=grid(4,5); 
+            C=grid(5,4); 
+            D=grid(5,5); 
+            Zp=[Zp, double(abs(A-B-C+D))]; 
+            E=grid(8,8); 
+            F=im(i+7,i+8); 
+            G=im(i+8,i+7); 
+            H=im(i+8,i+8); 
+            Zpp=[Zpp, double(abs(E-F-G+H))]; 
+        end 
+    end 
+    % 2)
+    figure 
+    HI=histogram(Zp/sum(Zp),255); 
+    hold on 
+    HII=histogram(Zpp/sum(Zpp),255); 
+    legend('Normalized center values','Normalized edge values')
+    % 3)
+    k=sum(abs(HI.Values-HII.Values)); 
+    n=0.25; 
+    % 4) 
+    jpegDetect = (k>n); 
+    fprintf("The strength of the blocking fingerprint is %d.\n",k); 
+    if jpegDetect==1
+        fprintf("So the image was JPEG compressed \n")
+    else
+        fprintf("So the image was not JPEG compressed \n")
+    end
+
+end
+
---
+++ ECES435/Assignment4/lab4.m
@@ -0,0 +1,24 @@
+% Feb 16, 2018 
+% Brian Hosler & Sarah Peachey 
+% Assignment 4 
+clear
+close all 
+clc
+%% Part 1 
+% extract the metadata tag associated with image 
+%   1) reading the quatization tables used to encode and decode in the
+%   jpeg header 
+%   2) compare to list of digital camera models 
+% Need wine to use JPEGsnoop.exe :( 
+
+
+%% Part 2 
+
+im1=imread('Assignment4Files/blockArtifacts1.tif');
+k1=blockDetect(im1)
+
+im2=imread('Assignment4Files/blockArtifacts2.tif');
+k1=blockDetect(im2)
+
+im3=imread('Assignment4Files/blockArtifacts3.tif');
+k1=blockDetect(im2)
