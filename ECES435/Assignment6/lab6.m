1b0b95e853b7cdf032ffc4ede6dfe768a173bff1
Brian Hosler <brian@drexel.edu>
2018-03-09 11:26:24 -0500

    Part1 Lab6 done

--- ECES435/Assignment6/lab6.m
+++ ECES435/Assignment6/lab6.m
@@ -1,5 +1,5 @@
 %Lab 6
-%Brian Hosler and Sarah Pidgey
+%Brian Hosler and Sarah Peachey
 ce1=imread('Assignment6Files/imageCE1.tif');
 ce2=imread('Assignment6Files/imageCE2.tif');
 ce3=imread('Assignment6Files/imageCE3.tif');
@@ -19,7 +19,7 @@ imhist(ce4)
 ui1=imread('Assignment6Files/unaltIm1.tif');
 ui2=imread('Assignment6Files/unaltIm2.tif');
 ui3=imread('Assignment6Files/unaltIm3.tif');
-for
+
 figure
 subplot(3,1,1)
 imhist(Gcorrection(ui1,.7))
@@ -28,4 +28,21 @@ imhist(ui1)
 subplot(3,1,3)
 imhist(Gcorrection(ui1,1.3))
 
+figure
+subplot(3,1,1)
+imhist(Gcorrection(ui2,.7))
+subplot(3,1,2)
+imhist(ui2)
+subplot(3,1,3)
+imhist(Gcorrection(ui2,1.3))
 
+figure
+subplot(3,1,1)
+imhist(Gcorrection(ui3,.7))
+subplot(3,1,2)
+imhist(ui3)
+subplot(3,1,3)
+imhist(Gcorrection(ui3,1.3))
+
+figure
+imhist(ce5)
